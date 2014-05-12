8d751d9d961f6597e9cecdb81814f1d3d47777c1
ceaparedes <vicea@alumnos.ubiobio.cl>
2014-05-12 18:29:20 -0300

    equals al punto

--- src/laberintoycasilla/Casilla.java
+++ src/laberintoycasilla/Casilla.java
@@ -4,6 +4,14 @@ package laberintoycasilla;
 public class Casilla {
     public int x;
     public int y;
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 43 * hash + this.x;
+        hash = 43 * hash + this.y;
+        return hash;
+    }
     
     public Casilla(int x, int y){
         this.x=x;
@@ -17,4 +25,11 @@ public class Casilla {
     public int getY() {
         return y;
     }
+    
+    @Override
+		    public boolean equals(Object obj) {
+		        if(!(obj instanceof Casilla)) return false;
+		        return ((Casilla)obj).getX() == this.getX() &&
+		                ((Casilla)obj).getY() == this.getY();
+		    }
 }
