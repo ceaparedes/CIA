edf168283b92a4858194192339a9d6a97f2675bc
Víctor Cea Paredes <vicea@alumnos.ubiobio.cl>
2014-05-13 03:18:28 -0300

    Update Busqueda.java

--- src/busquedayestados/Busqueda.java
+++ src/busquedayestados/Busqueda.java
@@ -2,112 +2,106 @@ package busquedayestados;
 
 import java.util.ArrayList;
 import laberintoycasilla.Casilla;
-
+import laberintoycasilla.Laberinto;
 
 public class Busqueda {
-
     private Estado actual, meta;
-    private ArrayList<Estado> abierto, cerrado;
-   
+    private ArrayList<Estado> abierto, cerrado; 
 
-    public Busqueda(Estado actual, Estado meta) {
+    
+    public Busqueda(Estado actual, Estado meta){
         this.actual = actual;
-        this.meta = meta;
-        
+        this.meta= meta;
         this.abierto = new ArrayList<>();
         this.cerrado = new ArrayList<>();
-
+        
     }
-
-    public String obtenerResultado() {
+    
+     public String obtenerResultado(){
         StringBuilder str;
         ArrayList<String> resultado = iniciarBusqueda();
         int tam;
-        if (resultado == null) {
-            return null;
-        }
+        
+        if(resultado ==null)return null;
+        
         str = new StringBuilder();
         tam = resultado.size();
-
-        for (int contador = 0; contador < tam; contador++) {
-            str.append("Paso ").append(contador + 1).append(", ")
+        
+        for(int contador =0; contador<tam;contador++){
+            str.append("Paso ").append(contador +1).append(", ")
                     .append(resultado.get(contador)).append("\n");
         }
+        
         return str.toString();
     }
 
-    public ArrayList iniciarBusqueda() {
+  
+    public ArrayList iniciarBusqueda(){
         boolean termino = false;
-        ArrayList<String> resultado;
-        ArrayList<Casilla> pos ;
+        ArrayList <String> resultado;
+        ArrayList <Casilla> posicion;
         abierto.add(actual);
-
-        while (!abierto.isEmpty() && !termino) {
-            actual = abierto.get(0);
-            if (!cerrado.contains(actual)) {
+       
+        while(!abierto.isEmpty() && !termino){
+            actual= abierto.get(0);
+            if (!cerrado.contains(actual)){
                 cerrado.add(actual);
-                
-                expandir(actual);
+                expandir (actual);
             }
-            if (actual.getPosicion().equals(meta.getPosicion())) {
-                termino = true;
+            if(actual.getPosicion1().equals(meta.getPosicion1())){
+                termino=true;
             }
-            abierto.remove(0);
-        }
-        if (!termino) {
-            return null;
-        }
-
-        resultado = new ArrayList<>();
-        pos = new ArrayList<>();
-
-        while (actual.getAntecesor() != null) {
-            resultado.add(0, actual.toString());
-            actual = actual.getAntecesor();
-            pos.add(actual.getPosicion());
-        }
+                abierto.remove(0);
+         }
+        if(!termino) return null;
         
-        System.out.Println(pos.get(0).getPosicion().x);
+        resultado = new ArrayList<>();
+        posicion = new ArrayList<>();
+     
+         while(actual.getAntecesor()!=null){
+         resultado.add(0,actual.toString());
+         posicion.add(0,actual.getPosicion());
+         actual = actual.getAntecesor();
+         }
+         System.out.println(posicion.get(0).x);
         return resultado;
     }
-
-    public void expandir(Estado actual) {
-        Casilla position;
-        Estado nuevo;
-
-        if (actual.moverArriba()) {
-            position = actual.getPosicion();
-            position.x -= 1;
-            nuevo = new Estado(position, actual, "Arriba");
-            
-            abierto.add(nuevo);
-
+    
+    public void expandir(Estado actual){
+         Casilla position;
+         Estado nuevo;
+         
+        if(actual.moverArriba()){   
+         position = actual.getPosicion();
+         position.x -=1;
+         nuevo = new Estado(position,actual,"se ha movido hacia Arriba"); 
+         abierto.add(nuevo);
+     
         }
-
-        if (actual.moverAbajo()) {
-            position = actual.getPosicion();
-            position.x += 1;
-            nuevo = new Estado(position, actual, "Abajo");
-            abierto.add(nuevo);
-
-        }
-
-        if (actual.moverIzquierda()) {
-            position = actual.getPosicion();
-            position.y -= 1;
-            nuevo = new Estado(position, actual, "Izquierda");
-            abierto.add(nuevo);
-
-        }
-
-        if (actual.moverDerecha()) {
-            position = actual.getPosicion();
-            position.y += 1;
-            nuevo = new Estado(position, actual, "Derecha");
-            abierto.add(nuevo);
+        
+        
+        if(actual.moverAbajo()){
+         position = actual.getPosicion();
+         position.x +=1;
+         nuevo = new Estado(position,actual,"se ha movido hacia Abajo");
+         abierto.add(nuevo);
 
         }
         
+        if(actual.moverIzquierda()){
+         position = actual.getPosicion();
+         position.y -=1;
+         nuevo = new Estado(position,actual,"se ha movido hacia la Izquierda");
+         abierto.add(nuevo);
+       
+        }
+        
+        if(actual.moverDerecha()){
+         position = actual.getPosicion();
+         position.y +=1;
+         nuevo = new Estado(position,actual,"se ha movido hacia la derecha");
+         abierto.add(nuevo);
+      
+        }
     }
-
-}
+    }  
